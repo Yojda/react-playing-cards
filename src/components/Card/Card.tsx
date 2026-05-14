ffc2afcdfc1f199df5866260f3e3d2a0cbe50237
Yojda <yanthekill@gmail.com>
2026-05-13 23:08:16 -0400

    fix: face down option

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,6 +6,7 @@ export interface CardProps {
   rank: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "T" | "J" | "Q" | "K" | "A";
   variant?: "primary" | "secondary" | "tertiary" | "quaternary";
   width?: number;
+  faceDown?: boolean;
   className?: string;
 }
 
@@ -16,17 +17,25 @@ export const Card: React.FC<CardProps> = ({
   rank,
   variant = "primary",
   width = 200,
+  faceDown = false,
   className = "",
 }) => {
   const normalizedRank = rank === "10" ? "T" : rank;
   const key = `${variant}-${normalizedRank}${suit.toLowerCase()}`; // e.g. "As", "Kd"
   const SvgComponent = SVG_MAP[key];
+  const faceDownKey = `back`;
+  const FaceDownComponent = SVG_MAP[faceDownKey];
 
   if (!SvgComponent) return <div>...</div>;
 
   return (
-    <div className={className} style={{ width }}>
-      <SvgComponent />
-    </div>
+    <>
+      {faceDown ? (
+        <FaceDownComponent width={width} className={className} />
+      ) : (
+        <SvgComponent width={width} className={className} />
+      )
+      }
+    </>
   );
 };
